3a375de42c7f426d6d673ff0af865549fdc6e2c5
Ryan Rosario <6713180+RyanRosario@users.noreply.github.com>
2026-01-14 21:32:52 +0000

    Add request enqueue latency metric

--- pkg/epp/metrics/metrics.go
+++ pkg/epp/metrics/metrics.go
@@ -399,6 +399,18 @@ var (
 		append([]string{"fairness_id", "priority", "outcome", "inference_pool"}, ModelLabels...),
 	)
 
+	flowControlRequestEnqueueDuration = prometheus.NewHistogramVec(
+		prometheus.HistogramOpts{
+			Subsystem: InferenceExtension,
+			Name:      "flow_control_request_enqueue_duration_seconds",
+			Help:      metricsutil.HelpMsgWithStability("Distribution of the time taken to enqueue requests by the EPP flow control layer.", compbasemetrics.ALPHA),
+			Buckets: []float64{
+				5, 10, 25, 50, 100, 250, 500, 1000,
+			},
+		},
+		[]string{"priority", "outcome"},
+	)
+
 	flowControlQueueSize = prometheus.NewGaugeVec(
 		prometheus.GaugeOpts{
 			Subsystem: InferenceExtension,
@@ -461,6 +473,7 @@ func Register(customCollectors ...prometheus.Collector) {
 		metrics.Registry.MustRegister(PrefixCacheHitLength)
 		metrics.Registry.MustRegister(flowControlRequestQueueDuration)
 		metrics.Registry.MustRegister(flowControlQueueSize)
+		metrics.Registry.MustRegister(flowControlRequestEnqueueDuration)
 		metrics.Registry.MustRegister(inferenceModelRewriteDecisionsTotal)
 		for _, collector := range customCollectors {
 			metrics.Registry.MustRegister(collector)
@@ -507,6 +520,7 @@ func Reset() {
 	PrefixCacheHitLength.Reset()
 	flowControlRequestQueueDuration.Reset()
 	flowControlQueueSize.Reset()
+	flowControlRequestEnqueueDuration.Reset()
 	inferenceModelRewriteDecisionsTotal.Reset()
 }
 
@@ -784,6 +798,16 @@ func RecordFlowControlRequestQueueDuration(
 		fairnessID, priority, outcome,
 		inferencePool,
 		modelName, targetModelName,
+	).Observe(duration.Seconds())
+}
+
+// RecordFlowControlRequestQueueDuration records the duration a request was in the enqueuing process in the Flow Control layer.
+func RecordFlowControlRequestEnqueueDuration(
+	priority string, outcome string,
+	duration time.Duration,
+) {
+	flowControlRequestEnqueueDuration.WithLabelValues(
+		priority, outcome,
 	).Observe(duration.Seconds())
 }
 
