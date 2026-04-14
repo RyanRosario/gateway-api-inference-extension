2ffd3ff0dc8382e6ee749f262908251987347b61
Ryan Rosario <6713180+RyanRosario@users.noreply.github.com>
2026-04-14 20:15:54 +0000

    Refactor to remove dependencies

--- pkg/lwepp/handlers/request.go
+++ pkg/lwepp/handlers/request.go
@@ -18,152 +18,62 @@ package handlers
 
 import (
 	"context"
-	"maps"
-	"strconv"
-	"time"
+	"net"
+	"strings"
+	"sync/atomic"
 
-	configPb "github.com/envoyproxy/go-control-plane/envoy/config/core/v3"
 	extProcPb "github.com/envoyproxy/go-control-plane/envoy/service/ext_proc/v3"
-	"go.opentelemetry.io/otel"
-	"go.opentelemetry.io/otel/propagation"
-	"google.golang.org/protobuf/types/known/structpb"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"sigs.k8s.io/controller-runtime/pkg/log"
 
 	envoy "sigs.k8s.io/gateway-api-inference-extension/pkg/common/envoy"
-	errcommon "sigs.k8s.io/gateway-api-inference-extension/pkg/common/error"
-	"sigs.k8s.io/gateway-api-inference-extension/pkg/epp/metadata"
-	"sigs.k8s.io/gateway-api-inference-extension/pkg/epp/util/request"
+	"sigs.k8s.io/gateway-api-inference-extension/pkg/lwepp/datastore"
 )
 
-func (s *StreamingServer) HandleRequestHeaders(ctx context.Context, reqCtx *RequestContext, req *extProcPb.ProcessingRequest_RequestHeaders) error {
-	reqCtx.RequestReceivedTimestamp = time.Now()
+func (s *StreamingServer) handleRequestHeaders(ctx context.Context, reqCtx *RequestContext, req *extProcPb.ProcessingRequest_RequestHeaders) error {
+	logger := log.FromContext(ctx)
 
-	// an EoS in the request headers means this request has no body or trailers.
-	if req.RequestHeaders.EndOfStream {
-		// We will route this request to a random endpoint as this is assumed to just be a GET
-		// More context: https://github.com/kubernetes-sigs/gateway-api-inference-extension/pull/526
-		// The above PR will address endpoint admission, but currently any request without a body will be
-		// routed to a random upstream endpoint.
-		endpoint := s.director.GetRandomEndpoint()
-		if endpoint == nil {
-			return errcommon.Error{Code: errcommon.Internal, Msg: "no pods available in datastore"}
-		}
-		reqCtx.TargetEndpoint = endpoint.GetIPAddress() + ":" + endpoint.GetPort()
-		reqCtx.RequestSize = 0
-		reqCtx.reqHeaderResp = s.generateRequestHeaderResponse(ctx, reqCtx)
-		return nil
-	}
-
+	var filterEndpoints []string
 	for _, header := range req.RequestHeaders.Headers.Headers {
-		reqCtx.Request.Headers[header.Key] = envoy.GetHeaderValue(header)
-		switch header.Key {
-		case metadata.FlowFairnessIDKey:
-			reqCtx.FairnessID = reqCtx.Request.Headers[header.Key]
-		case metadata.ObjectiveKey:
-			reqCtx.ObjectiveKey = reqCtx.Request.Headers[header.Key]
-		case metadata.ModelNameRewriteKey:
-			reqCtx.TargetModelName = reqCtx.Request.Headers[header.Key]
+		if header.Key == "test-epp-endpoint-selection" {
+			val := envoy.GetHeaderValue(header)
+			if val != "" {
+				filterEndpoints = strings.Split(val, ",")
+			}
 		}
 	}
 
-	if reqCtx.FairnessID == "" {
-		reqCtx.FairnessID = metadata.DefaultFairnessID
+	allPods := s.datastore.PodList(datastore.AllPodsPredicate)
+	if len(allPods) == 0 {
+		return status.Errorf(codes.Unavailable, "no pods available")
 	}
+
+	var candidates []*datastore.Endpoint
+	if len(filterEndpoints) > 0 {
+		for _, pod := range allPods {
+			for _, filter := range filterEndpoints {
+				if pod.Address == strings.TrimSpace(filter) {
+					candidates = append(candidates, pod)
+					break
+				}
+			}
+		}
+	}
+
+	// If no matches or header not present, use all pods
+	if len(candidates) == 0 {
+		candidates = allPods
+	}
+
+	// Round-robin selection
+	index := atomic.AddUint64(&s.rrIndex, 1)
+	selectedPod := candidates[index%uint64(len(candidates))]
+
+	reqCtx.SelectedPodIP = selectedPod.Address
+	reqCtx.TargetEndpoint = net.JoinHostPort(selectedPod.Address, selectedPod.Port)
+
+	logger.Info("Selected endpoint", "podIP", selectedPod.Address, "endpoint", reqCtx.TargetEndpoint)
 
 	return nil
-}
-
-func (s *StreamingServer) generateRequestHeaderResponse(ctx context.Context, reqCtx *RequestContext) *extProcPb.ProcessingResponse {
-	// The Endpoint Picker supports two approaches to communicating the target endpoint, as a request header
-	// and as an unstructure ext-proc response metadata key/value pair. This enables different integration
-	// options for gateway providers.
-	dynamicMetadata := s.generateMetadata(reqCtx.TargetEndpoint)
-	if reqCtx.Response.DynamicMetadata != nil {
-		if dynamicMetadata.Fields == nil {
-			dynamicMetadata.Fields = make(map[string]*structpb.Value)
-		}
-		maps.Copy(dynamicMetadata.Fields, reqCtx.Response.DynamicMetadata.Fields)
-	}
-
-	return &extProcPb.ProcessingResponse{
-		Response: &extProcPb.ProcessingResponse_RequestHeaders{
-			RequestHeaders: &extProcPb.HeadersResponse{
-				Response: &extProcPb.CommonResponse{
-					ClearRouteCache: true,
-					HeaderMutation: &extProcPb.HeaderMutation{
-						SetHeaders: s.generateHeaders(ctx, reqCtx),
-					},
-				},
-			},
-		},
-		DynamicMetadata: dynamicMetadata,
-	}
-}
-
-func (s *StreamingServer) generateHeaders(ctx context.Context, reqCtx *RequestContext) []*configPb.HeaderValueOption {
-	// can likely refactor these two bespoke headers to be updated in PostDispatch, to centralize logic.
-	headers := []*configPb.HeaderValueOption{
-		{
-			Header: &configPb.HeaderValue{
-				Key:      metadata.DestinationEndpointKey,
-				RawValue: []byte(reqCtx.TargetEndpoint),
-			},
-		},
-	}
-	if reqCtx.RequestSize > 0 {
-		// We need to update the content length header if the body is mutated, see Envoy doc:
-		// https://www.envoyproxy.io/docs/envoy/latest/api-v3/extensions/filters/http/ext_proc/v3/processing_mode.proto
-		headers = append(headers, &configPb.HeaderValueOption{
-			Header: &configPb.HeaderValue{
-				Key:      "Content-Length",
-				RawValue: []byte(strconv.Itoa(reqCtx.RequestSize)),
-			},
-		})
-	}
-
-	// Inject trace context headers for propagation to downstream services
-	traceHeaders := make(map[string]string)
-	propagator := otel.GetTextMapPropagator()
-	propagator.Inject(ctx, propagation.MapCarrier(traceHeaders))
-	for key, value := range traceHeaders {
-		headers = append(headers, &configPb.HeaderValueOption{
-			Header: &configPb.HeaderValue{
-				Key:      key,
-				RawValue: []byte(value),
-			},
-		})
-	}
-
-	// Include any non-system-owned headers.
-	for key, value := range reqCtx.Request.Headers {
-		if request.IsSystemOwnedHeader(key) {
-			continue
-		}
-		headers = append(headers, &configPb.HeaderValueOption{
-			Header: &configPb.HeaderValue{
-				Key:      key,
-				RawValue: []byte(value),
-			},
-		})
-	}
-	return headers
-}
-
-func (s *StreamingServer) generateMetadata(endpoint string) *structpb.Struct {
-	return &structpb.Struct{
-		Fields: map[string]*structpb.Value{
-			metadata.DestinationEndpointNamespace: {
-				Kind: &structpb.Value_StructValue{
-					StructValue: &structpb.Struct{
-						Fields: map[string]*structpb.Value{
-							metadata.DestinationEndpointKey: {
-								Kind: &structpb.Value_StringValue{
-									StringValue: endpoint,
-								},
-							},
-						},
-					},
-				},
-			},
-		},
-	}
 }
--- pkg/lwepp/handlers/request_test.go
+++ pkg/lwepp/handlers/request_test.go
@@ -23,130 +23,98 @@ import (
 	configPb "github.com/envoyproxy/go-control-plane/envoy/config/core/v3"
 	extProcPb "github.com/envoyproxy/go-control-plane/envoy/service/ext_proc/v3"
 	"github.com/stretchr/testify/assert"
-	"google.golang.org/protobuf/types/known/structpb"
-	"sigs.k8s.io/gateway-api-inference-extension/pkg/epp/metadata"
+
+	"sigs.k8s.io/gateway-api-inference-extension/pkg/lwepp/datastore"
 )
 
-func TestHandleRequestHeaders(t *testing.T) {
-	t.Parallel()
-
-	tests := []struct {
-		name           string
-		headers        []*configPb.HeaderValue
-		wantHeaders    map[string]string
-		wantFairnessID string
-	}{
-		{
-			name: "Extracts Fairness ID and Removes Header",
-			headers: []*configPb.HeaderValue{
-				{Key: "x-test", Value: "val"},
-				{Key: metadata.FlowFairnessIDKey, Value: "user-123"},
-			},
-			wantHeaders:    map[string]string{"x-test": "val"},
-			wantFairnessID: "user-123",
-		},
-		{
-			name: "Prefers RawValue over Value",
-			headers: []*configPb.HeaderValue{
-				{Key: metadata.FlowFairnessIDKey, RawValue: []byte("binary-id"), Value: "wrong-id"},
-			},
-			wantFairnessID: "binary-id",
-		},
-	}
-
-	for _, tc := range tests {
-		t.Run(tc.name, func(t *testing.T) {
-			server := &StreamingServer{}
-			reqCtx := &RequestContext{
-				Request: &Request{Headers: make(map[string]string)},
-			}
-			req := &extProcPb.ProcessingRequest_RequestHeaders{
-				RequestHeaders: &extProcPb.HttpHeaders{
-					Headers: &configPb.HeaderMap{Headers: tc.headers},
-				},
-			}
-
-			err := server.HandleRequestHeaders(context.Background(), reqCtx, req)
-			assert.NoError(t, err, "HandleRequestHeaders should not return an error")
-
-			assert.Equal(t, tc.wantFairnessID, reqCtx.FairnessID, "FairnessID should match expected value")
-
-			if tc.wantHeaders != nil {
-				for k, v := range tc.wantHeaders {
-					assert.Equal(t, v, reqCtx.Request.Headers[k], "Header %q should match expected value", k)
-				}
-			}
-		})
-	}
+type mockDatastore struct {
+	pods []*datastore.Endpoint
 }
 
-func TestGenerateHeaders_Sanitization(t *testing.T) {
-	server := &StreamingServer{}
-	reqCtx := &RequestContext{
-		TargetEndpoint: "1.2.3.4:8080",
-		RequestSize:    123,
-		Request: &Request{
-			Headers: map[string]string{
-				"x-user-data":                   "important",              // should passthrough
-				metadata.ObjectiveKey:           "sensitive-objective-id", // should be stripped
-				metadata.DestinationEndpointKey: "1.1.1.1:666",            // should be stripped
-				"content-length":                "99999",                  // should be stripped (re-added by logic)
-			},
-		},
-	}
-
-	results := server.generateHeaders(context.Background(), reqCtx)
-
-	gotHeaders := make(map[string]string)
-	for _, h := range results {
-		gotHeaders[h.Header.Key] = string(h.Header.RawValue)
-	}
-
-	assert.Contains(t, gotHeaders, "x-user-data")
-	assert.NotContains(t, gotHeaders, metadata.ObjectiveKey)
-	assert.Equal(t, "1.2.3.4:8080", gotHeaders[metadata.DestinationEndpointKey])
-	assert.Equal(t, "123", gotHeaders["Content-Length"])
+func (m *mockDatastore) PoolGet() (*datastore.EndpointPool, error) {
+	return nil, nil
 }
 
-func TestGenerateRequestHeaderResponse_MergeMetadata(t *testing.T) {
-	t.Parallel()
+func (m *mockDatastore) PodList(predicate func(*datastore.Endpoint) bool) []*datastore.Endpoint {
+	var res []*datastore.Endpoint
+	for _, p := range m.pods {
+		if predicate(p) {
+			res = append(res, p)
+		}
+	}
+	return res
+}
 
-	server := &StreamingServer{}
-	reqCtx := &RequestContext{
-		TargetEndpoint: "1.2.3.4:8080",
-		Request: &Request{
-			Headers: make(map[string]string),
+func TestHandleRequestHeaders_RoundRobin(t *testing.T) {
+	pods := []*datastore.Endpoint{
+		{Address: "10.0.0.1", Port: "8080"},
+		{Address: "10.0.0.2", Port: "8080"},
+	}
+	ds := &mockDatastore{pods: pods}
+	server := &StreamingServer{datastore: ds}
+
+	req := &extProcPb.ProcessingRequest_RequestHeaders{
+		RequestHeaders: &extProcPb.HttpHeaders{
+			Headers: &configPb.HeaderMap{},
 		},
-		Response: &Response{
-			DynamicMetadata: &structpb.Struct{
-				Fields: map[string]*structpb.Value{
-					"existing_namespace": {
-						Kind: &structpb.Value_StructValue{
-							StructValue: &structpb.Struct{
-								Fields: map[string]*structpb.Value{
-									"existing_key": {Kind: &structpb.Value_StringValue{StringValue: "existing_value"}},
-								},
-							},
-						},
-					},
+	}
+
+	// First request
+	reqCtx1 := &RequestContext{}
+	err := server.handleRequestHeaders(context.Background(), reqCtx1, req)
+	assert.NoError(t, err)
+
+	// Second request
+	reqCtx2 := &RequestContext{}
+	err = server.handleRequestHeaders(context.Background(), reqCtx2, req)
+	assert.NoError(t, err)
+
+	// They should be different pods (round-robin)
+	assert.NotEqual(t, reqCtx1.SelectedPodIP, reqCtx2.SelectedPodIP)
+
+	// Third request should wrap around
+	reqCtx3 := &RequestContext{}
+	err = server.handleRequestHeaders(context.Background(), reqCtx3, req)
+	assert.NoError(t, err)
+	assert.Equal(t, reqCtx1.SelectedPodIP, reqCtx3.SelectedPodIP)
+}
+
+func TestHandleRequestHeaders_Filtering(t *testing.T) {
+	pods := []*datastore.Endpoint{
+		{Address: "10.0.0.1", Port: "8080"},
+		{Address: "10.0.0.2", Port: "8080"},
+		{Address: "10.0.0.3", Port: "8080"},
+	}
+	ds := &mockDatastore{pods: pods}
+	server := &StreamingServer{datastore: ds}
+
+	req := &extProcPb.ProcessingRequest_RequestHeaders{
+		RequestHeaders: &extProcPb.HttpHeaders{
+			Headers: &configPb.HeaderMap{
+				Headers: []*configPb.HeaderValue{
+					{Key: "test-epp-endpoint-selection", Value: "10.0.0.2"},
 				},
 			},
 		},
 	}
 
-	resp := server.generateRequestHeaderResponse(context.Background(), reqCtx)
+	reqCtx := &RequestContext{}
+	err := server.handleRequestHeaders(context.Background(), reqCtx, req)
+	assert.NoError(t, err)
+	assert.Equal(t, "10.0.0.2", reqCtx.SelectedPodIP)
+}
 
-	// Check that the existing metadata is preserved
-	existingNamespace, ok := resp.DynamicMetadata.Fields["existing_namespace"]
-	assert.True(t, ok, "Expected existing_namespace to be in DynamicMetadata")
-	existingKey, ok := existingNamespace.GetStructValue().Fields["existing_key"]
-	assert.True(t, ok, "Expected existing_key to be in existing_namespace")
-	assert.Equal(t, "existing_value", existingKey.GetStringValue(), "Unexpected value for existing_key")
+func TestHandleRequestHeaders_NoPods(t *testing.T) {
+	ds := &mockDatastore{pods: []*datastore.Endpoint{}}
+	server := &StreamingServer{datastore: ds}
 
-	// Check that the new metadata is added
-	endpointNamespace, ok := resp.DynamicMetadata.Fields[metadata.DestinationEndpointNamespace]
-	assert.True(t, ok, "Expected DestinationEndpointNamespace to be in DynamicMetadata")
-	endpointKey, ok := endpointNamespace.GetStructValue().Fields[metadata.DestinationEndpointKey]
-	assert.True(t, ok, "Expected DestinationEndpointKey to be in DestinationEndpointNamespace")
-	assert.Equal(t, "1.2.3.4:8080", endpointKey.GetStringValue(), "Unexpected value for DestinationEndpointKey")
+	req := &extProcPb.ProcessingRequest_RequestHeaders{
+		RequestHeaders: &extProcPb.HttpHeaders{
+			Headers: &configPb.HeaderMap{},
+		},
+	}
+
+	reqCtx := &RequestContext{}
+	err := server.handleRequestHeaders(context.Background(), reqCtx, req)
+	assert.Error(t, err)
 }
